faf45f8f5a88ee1de76f3ac5c726544479ddb014
Diego Henrique <diego.henri94@gmail.com>
2021-05-04 00:07:41 -0300

    first commit

---
+++ app/src/test/kotlin/com/vibbra/timesheet/TimesheetBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vibbra.timesheet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimesheetBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
